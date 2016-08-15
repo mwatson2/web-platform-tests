d796ed7ecb8d276b9e5da5ca93ff0a3bcd734a5c
Mark Watson <watsonm@netflix.com>
2016-08-15 16:14:59 -0700

    Fixes for polyfills

--- encrypted-media/polyfill/edge-polyfill.js
+++ encrypted-media/polyfill/edge-polyfill.js
@@ -1,59 +1,180 @@
 (function() {
 
-    if ( navigator.userAgent.toLowerCase().indexOf('edge') > -1) ) {
+    if ( navigator.userAgent.toLowerCase().indexOf('edge') > -1 ) {
+    
+        var _mediaKeySystemAccessCreateMediaKeys = MediaKeySystemAccess.prototype.createMediaKeys;
+            _mediaKeysCreateSession = MediaKeys.prototype.createSession;
+    
+        // MediaKeySession proxy
+        function MediaKeySession( mediaKeys, session )
+        {
+            EventTarget.call( this );
 
-        var _mediaKeySystemAccessCreateMediaKeys = MediakKeySystemAccess.prototype.createMediaKeys;
-            _mediaKeysCreateSession = MediaKeys.prototype.createSession,
-            _mediaKeySessionLoad = MediaKeySession.prototype.load,
-            _mediaKeySessionUpdate = MediaKeySession.prototype.update,
-            _mediaKeySessionRemove = MediaKeySession.prototype.remove;
+            this._mediaKeys = mediaKeys;
+            this._session = session;
+            this._sessionId = undefined;
+            this._removing = false;
+            
+            session.addEventListener( 'message', this.dispatchEvent.bind( this ) );
+            session.addEventListener( 'keystatuseschange', this.dispatchEvent.bind( this ) );
+            session.closed.then( function() { if ( !this._removing ) this._resolveClosed(); }.bind ( this ) );
+
+            this._closed = new Promise( function( resolve ) { this._resolveClosed = resolve; }.bind( this ) );
+        }
+
+        MediaKeySession.prototype = Object.create( EventTarget.prototype );
+
+        Object.defineProperties( MediaKeySession.prototype, {
+            sessionId:  { get: function() { return this._sessionId ? this._sessionId : this._session.sessionId; } },
+            expiration: { get: function() { return this._session.expiration; } },
+            closed:     { get: function() { return this._closed; } },
+            keyStatuses:{ get: function() { return this._session.keyStatuses; } }
+        });
+
+        // load()
+        //
+        // Use a surrogate 'persistent-release-message' session to obtain the release message
+        //
+        MediaKeySession.prototype.load = function load( sessionId )
+        {
+            if ( this.sessionId ) return Promise.reject( new DOMException('InvalidAccessError') );
+            
+            this._surrogate = this._mediaKeys.createSession( 'persistent-release-message' );
+            this._surrogate.addEventListener( 'message', this.dispatchEvent.bind( this ) );
+ 
+            return this._surrogate.load( sessionId ).then( function( success ) {
+                if (!success) return false;
+                
+                this._sessionId = sessionId;
+                this._removing = true;
+                this._session.close();
+                
+                return this._surrogate.remove().then( function() { return true; } );
+            }.bind( this ) );
+        };
+    
+        // remove()
+        //
+        // On an existing session, use a surrogate 'persistent-release-message' session to obtain the release message
+        //
+        MediaKeySession.prototype.remove = function remove()
+        {
+            if ( this._sessionId !== undefined ) return Promise.reject( new DOMException('InvalidAccessError') );
+            if ( this.sessionId === undefined ) return Promise.reject( new DOMException('InvalidAccessError') );
+                
+            this._surrogate = this._mediaKeys.createSession( 'persistent-release-message' );
+            this._surrogate.addEventListener( 'message', this.dispatchEvent.bind( this ) );
+            this._removing = true;
+            this._session.close();
+            
+            return this._surrogate.load( this.sessionId ).then( function( success ) {
+                if ( !success ) return Promise.reject( new DOMException('InvalidAccessError') );
+                
+                return this._surrogate.remove().then( function() { return true; } );
+            }.bind( this ) );
+        };
+    
+        // update()
+        //
+        // For a normal session, pass through, otherwise update the surrogate and close the proxy
+        MediaKeySession.prototype.update = function update( message )
+        {
+            if ( !this._removing ) return this._session.update( message );
+            
+            return this._surrogate.update( message ).then( function() {
+                this._resolveClosed();
+            }.bind( this ) );
+        };
+    
+        // close() - pass through
+        //
+        MediaKeySession.prototype.close = function close()
+        {
+            if (!this._removing) return this._session.close();
+            this._resolveClosed();
+            return Promise.resolve();
+        };
+    
+        // generateRequest() - pass through
+        //
+        MediaKeySession.prototype.generateRequest = function generateRequest( initDataType, initData )
+        {
+            if ( this.sessionId ) Promise.reject( new DOMException('InvalidAccessError') );
+            return this._session.generateRequest( initDataType, initData );
+        };
+    
+        // Wrap PlayReady persistent-usage-record sessions in our Proxy
+        MediaKeys.prototype.createSession = function createSession( sessionType ) {
+ 
+            var session = _mediaKeysCreateSession.call( this, sessionType );
+            if ( this._keySystem !== 'com.microsoft.playready' || sessionType !== 'persistent-usage-record' )
+            {
+                return session;
+            }
+ 
+            return new MediaKeySession( this, session );
+
+        };
+    
+        //
+        // Annotation polyfills - annotate not otherwise available data
+        //
     
         // Annotate MediaKeys with the keysystem
-        MediaKeySystemAccess.prototype.createMediaKeys = function createMediaKeys() {
+        MediaKeySystemAccess.prototype.createMediaKeys = function createMediaKeys()
+        {
             return _mediaKeySystemAccessCreateMediaKeys.call( this ).then( function( mediaKeys ) {
-                mediaKeys._playready = ( this.keySystem === 'com.microsoft.playready' );
+                mediaKeys._keySystem = this.keySystem;
                 return mediaKeys;
             }.bind( this ) );
         };
     
-        // Annotate the session with the session type
-        MediaKeys.prototype.createSession = function createSession( sessionType ) {
-            var actualSessionType = ( sessionType === 'persistent-usage-record' ) ? 'persistent-release-message' : sessionType;
-            var session = _mediaKeysCreateSession.call( this, actualSessionType );
-            if ( this._playready && sessionType === 'persistent-usage-record' ) session._playready_pur = true;
-            return session;
+        //
+        // Utilities
+        //
+        
+        // Allow us to modify the target of Events
+        Object.defineProperties( Event.prototype, {
+            target: {   get: function() { return this._target || this.currentTarget; },
+                        set: function( newtarget ) { this._target = newtarget; } }
+        } );
+
+        // Make an EventTarget base class
+        function EventTarget(){
+            this.listeners = {};
         };
-    
-        // Call remove automatically on load for PlayReady persistent-usage-record sessions
-        MediaKeySession.prototype.load = function load( sessionId )
-        {
-            _mediaKeySessionLoad._loaded = true;
-            return _mediaKeySessionLoad.call( this, sessionId ).then( function( success ) {
-                if ( success && this._playready_pur )
-                {
-                    return this.remove().then( function() { return true; } );
+
+        EventTarget.prototype.listeners = null;
+
+        EventTarget.prototype.addEventListener = function(type, callback){
+            if(!(type in this.listeners)) {
+            this.listeners[type] = [];
+            }
+            this.listeners[type].push(callback);
+        };
+
+        EventTarget.prototype.removeEventListener = function(type, callback){
+            if(!(type in this.listeners)) {
+                return;
+            }
+            var stack = this.listeners[type];
+            for(var i = 0, l = stack.length; i < l; i++){
+                if(stack[i] === callback){
+                    stack.splice(i, 1);
+                    return this.removeEventListener(type, callback);
                 }
-                return success;
-            }.bind( this ) );
-        };
-    
-        // Annote the session if we have called remove on a PlayReady persistent-usage-record session
-        MediaKeySession.prototype.remove = function remove()
-        {
-            // Return an error on a pur session that was loaded
-            if ( this._playready_pur && this._loaded ) return Promise.reject( new Exception('InvalidAccessError') );
-            if ( this._playready_pur ) {
-                // Work around absense of in-session remove functionality
-            
             }
         };
-    
-        // Close the session automatically on update after remove on a PlayReady persistent-usage-record session
-        MediaKeySession.prototype.update = function update( message )
-        {
-            return _mediaKeySessionUpdate.call( this, message ).then( function() {
-                if ( this._remove ) return this.close();
-            }.bind( this ) );
+
+        EventTarget.prototype.dispatchEvent = function(event){
+            if(!(event.type in this.listeners)) {
+                return;
+            }
+            var stack = this.listeners[event.type];
+            event.target = this;
+            for(var i = 0, l = stack.length; i < l; i++) {
+                stack[i].call(this, event);
+            }
         };
     }
 })();
